9ed09eac557a506de0a56fe7fdb92e9e02bfff5f
romanf77 <romanfloris77@gmail.com>
2017-04-11 11:21:01 -0300

    Ejercicio Colecciones

---
+++ Clase 7/Colecciones/noGenerics/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noGenerics
+{
+    public class Producto
+    {
+        public int Codigo;
+        public string Descripcion;
+
+        public void Mostrar()
+        {
+            Console.WriteLine(this.Codigo);
+            Console.WriteLine(this.Descripcion);
+        
+        }
+    }
+}
---
+++ Clase 7/Colecciones/noGenerics/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList miArray;
+
+            miArray = new ArrayList(2);
+
+            Producto miProducto;
+            
+            miProducto = new Producto();
+
+            miArray.Add(miProducto);
+
+            miArray.Add(3);
+                     
+            foreach (Producto item in miArray)
+            {
+                //item.Mostrar(); Esto genera error al no ser todos del mismo tipo              
+            }
+
+            Stack Pila = new Stack();
+
+            Pila.Push(1);
+
+            Pila.Pop();
+
+            Pila.Peek();
+
+            Queue Cola = new Queue();
+
+            Cola.Peek();
+
+            Cola.Dequeue();
+
+            Cola.Enqueue(1);
+
+
+
+
+
+
+
+
+
+
+            
+
+            
+           
+
+        }
+    }
+}
