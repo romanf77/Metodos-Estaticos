c447ab6227d20bc0aba84c865c3af4797ac698c5
romanf77 <romanfloris77@gmail.com>
2017-04-18 11:54:06 -0300

    Indexadores

---
+++ Clase 8 Array/PROG-LAB-II-master/Clase 08/ejemplosArray/Indexadores/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexadores
+{
+    class Alumno
+    {
+        private int _Legajo;
+        private string _Nombre;
+
+        public Alumno(int Legajo)
+        { 
+            this._Legajo=Legajo;
+        
+        
+        }
+
+        public Alumno(int Legajo, string Nombre): this(Legajo)
+        {
+            this._Nombre = Nombre;
+        
+        }
+
+    }
+}
---
+++ Clase 8 Array/PROG-LAB-II-master/Clase 08/ejemplosArray/Indexadores/Aula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexadores
+{
+    class Aula
+    {
+        private int _NumeroDeAula;
+        private List<Alumno> _ListadoDeAlumnos;
+
+        public Aula()
+        {
+            this._ListadoDeAlumnos = new List<Alumno>();
+        
+        }
+
+        public Alumno this[int Indice]
+        {
+            get { return this._ListadoDeAlumnos[Indice]; }
+            set { }
+        
+        }
+
+    }
+}
